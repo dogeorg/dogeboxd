a4798286d2865e7c51a23c2c239cc0c956637355
tjstebbing <tim@dogecoin.com>
2024-04-03 10:33:36 +1100

    added cors middleware

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/dogeorg/dogeboxd
 
 go 1.22.1
+
+require github.com/rs/cors v1.10.1 // indirect
--- pkg/rest.go
+++ pkg/rest.go
@@ -8,6 +8,7 @@ import (
 	"net/http"
 
 	"github.com/dogeorg/dogeboxd/pkg/conductor"
+	"github.com/rs/cors"
 )
 
 func RESTAPI(config ServerConfig, state State) conductor.Service {
@@ -36,7 +37,8 @@ func (t api) getManifest(w http.ResponseWriter, r *http.Request) {
 
 func (t api) Run(started, stopped chan bool, stop chan context.Context) error {
 	go func() {
-		srv := &http.Server{Addr: fmt.Sprintf("%s:%d", t.config.Bind, t.config.Port), Handler: t.mux}
+		handler := cors.Default().Handler(t.mux)
+		srv := &http.Server{Addr: fmt.Sprintf("%s:%d", t.config.Bind, t.config.Port), Handler: handler}
 		go func() {
 			if err := srv.ListenAndServe(); err != http.ErrServerClosed {
 				log.Fatalf("HTTP server public ListenAndServe: %v", err)
@@ -59,7 +61,6 @@ func sendResponse(w http.ResponseWriter, payload any) {
 		sendErrorResponse(w, http.StatusInternalServerError, fmt.Sprintf("in json.Marshal: %s", err.Error()))
 		return
 	}
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-store") // do not cache (Browsers cache GET forever by default)
 	w.Write(b)
@@ -70,7 +71,6 @@ func sendErrorResponse(w http.ResponseWriter, code int, message string) {
 	// would prefer to use json.Marshal, but this avoids the need
 	// to handle encoding errors arising from json.Marshal itself!
 	payload := fmt.Sprintf("{\"error\":{\"code\":%q,\"message\":%q}}", code, message)
-	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Cache-Control", "no-store") // do not cache (Browsers cache GET forever by default)
 	w.WriteHeader(code)
