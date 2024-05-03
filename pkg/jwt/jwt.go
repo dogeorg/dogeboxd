9853fdaba0bde9a685fa664ce270969f9b240d88
blend <151626101+blendtwenty@users.noreply.github.com>
2024-05-03 14:31:55 +1000

    adds a login handler testing provided password against stored password

---
+++ pkg/jwt/jwt.go
@@ -0,0 +1,6 @@
+package jwt
+
+// TODO: implement actual (use something battle tested)
+func GenerateToken(userDetails interface{}) (string, error) {
+  return "peanut-butter", nil
+}
--- pkg/rest.go
+++ pkg/rest.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 
 	"github.com/dogeorg/dogeboxd/pkg/conductor"
+	"github.com/dogeorg/dogeboxd/pkg/jwt"
 	"github.com/rs/cors"
 )
 
@@ -19,6 +20,7 @@ func RESTAPI(config ServerConfig, dbx Dogeboxd, ws WSRelay) conductor.Service {
 		"GET /bootstrap/": a.getBootstrap,
 		//"GET /pup/status":      a.getPupStatus,
 		"POST /config/{PupID}": a.updateConfig,
+		"POST /auth/login": a.attemptLogin,
 		"/ws/state/": ws.GetWSHandler(func() any {
 			return Change{ID: "internal", Error: "", Type: "bootstrap", Update: a.getRawBS()}
 		}).ServeHTTP,
@@ -65,6 +67,58 @@ func (t api) updateConfig(w http.ResponseWriter, r *http.Request) {
 	}
 	id := t.dbx.AddAction(UpdatePupConfig{PupID: pupid, Payload: data})
 	sendResponse(w, map[string]string{"id": id})
+}
+
+func (t api) attemptLogin(w http.ResponseWriter, r *http.Request) {
+  // Read the request body
+  body, err := ioutil.ReadAll(r.Body)
+  if err != nil {
+    sendErrorResponse(w, http.StatusBadRequest, "Bad Request")
+    return
+  }
+  // Not done with the body yet.
+  defer r.Body.Close()
+
+  // Parse the request body
+  var credentials struct { Password string `json:"password"` }
+  err = json.Unmarshal(body, &credentials)
+  if err != nil {
+    sendErrorResponse(w, http.StatusBadRequest, "Error unmarshalling JSON, expects { password: 'value' }")
+    return
+  }
+
+  // Retrieve the stored password from dogeboxd
+  dogePupStatus, ok := t.dbx.Pups["internal.dogeboxd"]
+	if !ok {
+		// TODO: something here
+    return
+	}
+
+	storedPassword, ok := dogePupStatus.Config["password"]
+	if !ok {
+    // TODO.  no password, awkward.
+    return
+	}
+
+  // Check for password match
+  if !validatePassword(string(credentials.Password), storedPassword) {
+    sendResponse(w, map[string]string{"error": "CHECK-CREDS"})
+    return
+  }
+
+  // Generate and return a JWT token
+  token, err := jwt.GenerateToken(nil /* hmm.. user details? */)
+  if err != nil {
+    sendErrorResponse(w, http.StatusInternalServerError, "Internal Server Error")
+    return
+  }
+
+  sendResponse(w, map[string]string{"token": token})
+}
+
+// TODO: shift away?
+func validatePassword(providedPassword, storedPassword string) bool {
+   return providedPassword == storedPassword
 }
 
 func (t api) Run(started, stopped chan bool, stop chan context.Context) error {
