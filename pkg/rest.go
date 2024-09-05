1f71196ccb5a26d8cc19a88fdc834af56f7e6b56
Adam Brady <adam@boxxen.org>
2024-09-05 10:48:37 +1000

    Inject currently-installed pups into store listing response

--- pkg/rest.go
+++ pkg/rest.go
@@ -709,15 +709,16 @@ func (t api) getStoreList(w http.ResponseWriter, r *http.Request) {
 	for k, source := range available {
 		pups := map[string]StoreListSourceEntryPup{}
 
+		// TODO: Ideally not have to do this lookup.
+		s, err := t.dbx.sources.GetSource(k)
+		if err != nil {
+			sendErrorResponse(w, http.StatusInternalServerError, "Error fetching source")
+			return
+		}
+
 		for _, availablePup := range source.Pups {
 			// Check if we already have a pup in our list for this version.
 			if _, ok := pups[availablePup.Name]; !ok {
-				// TODO: Ideally not have to do this lookup.
-				s, err := t.dbx.sources.GetSource(k)
-				if err != nil {
-					sendErrorResponse(w, http.StatusInternalServerError, "Error fetching source")
-					return
-				}
 
 				// Check in our pup manager to see if this pup is installed.
 				// If it is, we set the InstalledVersion.
@@ -744,6 +745,23 @@ func (t api) getStoreList(w http.ResponseWriter, r *http.Request) {
 			pupEntry := pups[availablePup.Name]
 			pupEntry.Versions[availablePup.Version] = availablePup.Manifest
 			pups[availablePup.Name] = pupEntry
+		}
+
+		// Override any entry in the listing with what we actually have installed.
+		// We want to show that is _actually_ installed, rather than what might have been removed or updated underneath us.
+		// nb. We don't let you remove a source if you have a pup installed from it, so this should be safe here.
+		for _, installedPup := range t.dbx.Pups.GetStateMap() {
+			if installedPup.Source.Location == s.Config().Location && installedPup.Source.Name == s.Config().Name {
+				if _, ok := pups[installedPup.Manifest.Meta.Name]; !ok {
+					pups[installedPup.Manifest.Meta.Name] = StoreListSourceEntryPup{
+						IsInstalled:      true,
+						InstalledVersion: installedPup.Version,
+						Versions:         map[string]pup.PupManifest{},
+					}
+				}
+
+				pups[installedPup.Manifest.Meta.Name].Versions[installedPup.Version] = installedPup.Manifest
+			}
 		}
 
 		response[k] = StoreListSourceEntry{
