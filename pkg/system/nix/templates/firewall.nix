092f7457e0a364dea9e7185d655107f9156f9fa7
Adam Brady <adam@boxxen.org>
2024-09-20 12:45:36 +1000

    temp: open 80 in firewall to allow for pup-router

--- pkg/system/nix/templates/firewall.nix
+++ pkg/system/nix/templates/firewall.nix
@@ -4,6 +4,10 @@
   networking.firewall.enable = true;
 
   networking.firewall.allowedTCPPorts = [
+    # Allow dogeboxd to bind to 80 for the pup-router.
+    # TODO: Make this an explicit firewall rule only available to pup cide.
+    80
+
     {{ if .SSH_ENABLED }}
     # TODO: Allow the user to customise this at some point.
     # Enable port 22 for OpenSSH
