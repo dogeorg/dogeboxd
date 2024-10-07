d8a4e88311473f9a6de63c067e4afb1a8f657b90
Adam Brady <adam@boxxen.org>
2024-10-07 19:22:01 +1100

    Only import pup files if they exist

--- pkg/system/nix/templates/dogebox.nix
+++ pkg/system/nix/templates/dogebox.nix
@@ -7,7 +7,8 @@
     ./network.nix
     # ./recovery_ap.nix
     ./system_container_config.nix
-    {{range .PUP_IDS}}./pup_{{.}}.nix
-    {{end}}
-  ];
+  ]
+  {{range .PUP_IDS}}++ lib.optionals (builtins.pathExists ./pup_{{.}}.nix) [ ./pup_{{.}}.nix ]
+  {{end}}
+  ;
 }
